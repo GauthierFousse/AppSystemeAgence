c917f89f1781ef428f165a6e157172d535f1842f
fp <fp@UX303LA>
2016-06-14 12:27:11 +0200

    Frantz : modif import javax.faces.bean

--- appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/managedBean/AgenceBean.java
+++ appSystemeAgence/src/main/java/com/adaming/appSystemeAgence/managedBean/AgenceBean.java
@@ -3,7 +3,8 @@ package com.adaming.appSystemeAgence.managedBean;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.faces.bean.*;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.@ManagedProperty;
 import javax.enterprise.context.RequestScoped;
 
 import com.adaming.appSystemeAgence.modele.Conseiller;
